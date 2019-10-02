ce22ca5ecf5db4ee3674c405a1ec21e990d77589
Stephen Sweetland <stephen.sweetland@triad.uk.com>
2019-10-02 01:06:47 +0100

    getting distracted. lets tdd

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const app = express();
-const CronJob = require('cron').CronJob;
+const cron = require('cron').CronJob;
 const fs = require('fs');
+const pagespeed_key = 'AIzaSyDHtEzg_UazWlbJ1IThSxHIJ36ESnqApR4';
 
 // bit meh
 const sites = ['https://www.hotel-internet-marketing.com', 'https://www.bbc.co.uk', 'https://www.google.com'];
+const data_file = 'data/sites.json';
+
+// data file doesn't exist? create it.
+try {
+	if (!fs.existsSync(data_file)) {
+		fs.writeFileSync(data_file, JSON.stringify([]));
+	}
+} catch(err) {
+	console.error(err)
+}
 
 // why not? periodically update the data.
-const updateSpeeds = new CronJob({
-	cronTime: '0 * * * * *',
+const update_data = new cron({
+	cronTime: '0 0 * * * *',
 	onTick: function() {
+		console.log('tick');
 		console.log(sites);
 	},
 	start: false,
-	timeZone: 'Europe/London'
+	timeZone: 'Europe/London',
+	runOnInit: true
 });
-updateSpeeds.start();
+update_data.start();
 
+// the endpoint to return the data.
 app.get('/sites', (request, response) => {
-	return response.send('get method');
+	response.send(JSON.parse(fs.readFileSync('data/sites.json')));
 })
 
 app.listen(8080, () => {
